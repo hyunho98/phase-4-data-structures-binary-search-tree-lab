3c3fc7c629b405e7fd5e0737b0cfe986d2b898e9
Alex Lee <hyunho98@gmail.com>
2023-11-23 21:28:49 -0500

    commit tree c8653a5aac192578b8a49d1c095a79d7e7bf1fa2

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -7,12 +7,54 @@ class BinarySearchTree
     @root = root
   end
 
-  def search(value)
+  def search(value, node = root)
     # your code here
+    if node.nil?
+      return nil
+    end
+
+    if value == node.value
+      return node
+    end
+
+    if node.left || node.right
+      value < node.value ? node = node.left : node = node.right
+      return search(value, node)
+    end
   end
 
   def insert(value)
     # your code here
-  end
+    newNode = Node.new(value)
+    node = root
 
+    if root.nil?
+      @root = newNode
+      return newNode
+    end
+
+    while true
+      if newNode.value == node.value
+        return nil
+      end
+
+      if newNode.value < node.value
+        if node.left.nil?
+          node.left = newNode
+          return newNode
+        end
+
+        node = node.left
+      end
+
+      if newNode.value > node.value
+        if node.right.nil?
+          node.right = newNode
+          return newNode
+        end
+
+        node = node.right
+      end
+    end
+  end
 end 
